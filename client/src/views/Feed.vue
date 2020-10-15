52c14a26651760cff68666cb4961a74ba3cd4950
Elijah Judge <elijahgjudge@gmail.com>
2020-10-14 20:41:14 -0400

    added basic design elements for feed

--- client/src/views/Feed.vue
+++ client/src/views/Feed.vue
@@ -1,15 +1,36 @@
 <template>
   <div class="page">
       <h2 class="title is-2">Feed Page</h2>
+
+    <div class="columns">
+
+        <div class="column is-two-thirds">
+            <Posts /> <Posts /> <Posts /> <Posts />
+        </div>
+
+        <div class="column is-one-third">
+            <Sidebar /> <Posts />
+        </div>
+
+    </div>
   </div>
 </template>
 
 <script>
-export default {
 
+import Sidebar from "@/components/Sidebar";
+import Posts from "@/components/Posts";
+
+
+export default {
+    components:{
+        Sidebar, Posts
+    }
 }
 </script>
 
 <style>
-
+    .card{
+        margin-top: 15px;
+    }
 </style>
